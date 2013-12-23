e8ea5efd8301d801b4464e2a0528cdee6cc0ae7b
area722 <wouter.waeytens@student.howest.be>
2013-12-23 18:16:39 +0100

    rest service basic & intial commit store app

---
+++ 2NMCT3_Waeytens_Wouter_BAP_01/PortableLibrary/Band.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace PortableLibrary
+{
+    public class Band
+    {
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private String _name;
+
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private String _phone;
+
+        public String Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        private String _fax;
+
+        public String Fax
+        {
+            get { return _fax; }
+            set { _fax = value; }
+        }
+
+        private String _email;
+
+        public String Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private Byte[] _photo;
+
+        public Byte[] Photo
+        {
+            get { return _photo; }
+            set { _photo = value; }
+        }
+
+        private String _description;
+
+        public String Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private String _twitter;
+
+        public String Twitter
+        {
+            get { return _twitter; }
+            set { _twitter = value; }
+        }
+
+        private String _facebook;
+
+        public String Facebook
+        {
+            get { return _facebook; }
+            set { _facebook = value; }
+        }
+
+        //private ObservableCollection<Genre> _genres;
+
+        //public ObservableCollection<Genre> Genres
+        //{
+        //    get { return _genres; }
+        //    set { _genres = value; }
+        //}
+    }
+}
---
+++ 2NMCT3_Waeytens_Wouter_BAP_01/REST_service/Models/DAL/BandRepo.cs
@@ -0,0 +1,38 @@
+﻿using DBHelper;
+using PortableLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace REST_service.Models.DAL
+{
+    public class BandRepo
+    {
+        public static List<Band> getBands()
+        {
+            List<Band> lst = new List<Band>();
+            string sql = "SELECT * FROM Bands";
+            DbDataReader reader = Database.GetData(sql);
+            while (reader.Read())
+            {
+                Band band = new Band()
+                {
+                    ID = (int)reader[0],
+                    Name = (string)reader[1],
+                    Phone = (string)reader[2],
+                    Fax = (string)reader[3],
+                    Email = (string)reader[4],
+                    Photo = (Byte[])reader[5],
+                    Description = (string)reader[6],
+                    Twitter = (string)reader[7],
+                    Facebook = (string)reader[8],
+                };
+               // band.Genres = getGenresByBandId(band);
+                lst.Add(band);
+            }
+            return lst;
+        }
+    }
+}
